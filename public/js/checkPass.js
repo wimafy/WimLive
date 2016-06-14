9e9fc6c75c133e92bd85cd6d379c58c1ef47ffef
Kenta <kentaito@southern.edu>
2016-06-14 16:10:47 -0400

    more live check changes

--- public/js/checkPass.js
+++ public/js/checkPass.js
@@ -4,30 +4,43 @@ function checkPass()
 	var pass1 = document.getElementById('password');
 	var pass2 = document.getElementById('password_confirmation');
 	//Store the Confimation Message Object ...
-	var message = document.getElementById('passwordMessage');
+	var checkMessage = document.getElementById('passwordMessage');
 	//Set the colors we will be using ...
 	var goodColor = "#66cc66";
 	var badColor = "#aaaaaa";
 	//Compare the values in the password field 
 	//and the confirmation field
-	if(pass1.value.length < 8){
-		pass1.style.backgroundColor = badColor;
-		message.style.color = badColor;
-		message.innerHTML = "Password is too short. Requires more than 8 characters.";
-	}
-	else if(pass1.value == pass2.value){
+	if(pass1.value == pass2.value){
 		//The passwords match. 
 		//Set the color to the good color and inform
 		//the user that they have entered the correct password 
 		pass2.style.backgroundColor = goodColor;
-		message.style.color = goodColor;
-		message.innerHTML = "Passwords match!"
+		checkMessage.style.color = goodColor;
+		checkMessage.innerHTML = "Passwords match!"
 	}else{
 		//The passwords do not match.
 		//Set the color to the bad color and
 		//notify the user.
 		pass2.style.backgroundColor = badColor;
-		message.style.color = badColor;
-		message.innerHTML = "Passwords do not match!"
+		checkMessage.style.color = badColor;
+		checkMessage.innerHTML = "Passwords do not match!"
+	}
+}
+
+function checkLength(){
+	var pass1 = document.getElementById('password');
+	var lengthMessage = document.getElementById('lengthMessage');
+	
+	var goodColor = "#66cc66";
+	var badColor = "#aaaaaa";
+	
+	if(pass1.value.length < 8){
+		pass1.style.backgroundColor = badColor;
+		lengthMessage.style.color = badColor;
+		lengthMessage.innerHTML = "Password is too short. Requires more than 8 characters.";
+	}
+	else{
+		pass1.style.backgroundColor = goodColor;
+		lengthMessage.innerHTML = "";
 	}
 }
