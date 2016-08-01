c271a0732cd98c3cae39e7a1fbb8b7ad4eb8783f
wimafy <wimafy@gmail.com>
2016-08-01 16:18:17 -0700

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -57,13 +57,14 @@
              
         <script>
             function checkifpageisloaded() {
-                console.log("HOLD UP");
             var everythingLoaded = setInterval(function() {
               if (/loaded|complete/.test(document.readyState)) {
                 clearInterval(everythingLoaded);
                 init(); // this is the function that gets called when everything is loaded
                 checkifpageisloaded();
 
+              } else {
+                  alert("notloaded");
               }
             }, 10);
             }
