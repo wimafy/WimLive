a1285e98c87c8840e59ce74d056c981cb20e0f06
wimafy <wimafy@gmail.com>
2016-08-10 18:00:09 -0700

    ripple.js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,87 +26,16 @@
 		<script src = "js/signupValidation.js"></script>
 		<script src = "js/dropdown.js"></script>
 		<script src = "js/profileActions.js"></script>
+        <script src = "js/ripple.js"></script>
 
         
-        
-        <!-- material design stuffs-->
-        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
-        
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-        <link href="dist/css/bootstrap-material-design.css" rel="stylesheet">
-        
-        <link href="dist/css/ripples.min.css" rel="stylesheet">
-        <link href="//fezvrasta.github.io/snackbarjs/dist/snackbar.min.css" rel="stylesheet">
-        <link href="//fezvrasta.github.io/snackbarjs/dist/snackbar.min.css" rel="stylesheet">
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         
         <!--loading screen icon -->
         <!-- old jquery (it works though)
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>-->
         <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
         <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
-        <script src="dist/js/ripples.min.js"></script>
-        <script src="dist/js/material.min.js"></script>
-        <script src="//fezvrasta.github.io/snackbarjs/dist/snackbar.min.js"></script>
-        <script src="//cdnjs.cloudflare.com/ajax/libs/noUiSlider/6.2.0/jquery.nouislider.min.js"></script>
-        <script>
-  (function () {
 
-    var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function () {
-      var index = $('.bs-component').index($(this).parent());
-      $.get(window.location.href, function (data) {
-        var html = $(data).find('.bs-component').eq(index).html();
-        html = cleanSource(html);
-        $("#source-modal pre").text(html);
-        $("#source-modal").modal();
-      })
-
-    });
-
-    $('.bs-component [data-toggle="popover"]').popover();
-    $('.bs-component [data-toggle="tooltip"]').tooltip();
-
-    $(".bs-component").hover(function () {
-      $(this).append($button);
-      $button.show();
-    }, function () {
-      $button.hide();
-    });
-
-    function cleanSource(html) {
-      var lines = html.split(/\n/);
-
-      lines.shift();
-      lines.splice(-1, 1);
-
-      var indentSize = lines[0].length - lines[0].trim().length,
-          re = new RegExp(" {" + indentSize + "}");
-
-      lines = lines.map(function (line) {
-        if (line.match(re)) {
-          line = line.substring(indentSize);
-        }
-
-        return line;
-      });
-
-      lines = lines.join("\n");
-
-      return lines;
-    }
-
-    $(".icons-material .icon").each(function () {
-      $(this).after("<br><br><code>" + $(this).attr("class").replace("icon ", "") + "</code>");
-    });
-
-  })();
-
-</script>
-        
-        
-        
         <script>
         //paste this code under the head tag or in a separate js file.
             // Wait for window load
