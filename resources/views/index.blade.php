c5d895330a887c3b841fee5c37502ee8f7672271
wimafy <wimafy@gmail.com>
2016-08-11 13:13:42 -0700

    Revert "css"
    This reverts commit 2187bad47407987d61009854df42b554c1370bd9.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,12 +29,9 @@
         
         
         
-        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        
-        
-        
+        <!--loading screen icon -->
+        <!-- old jquery (it works though)
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>-->
         <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
         <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
 
