7b5b855491321125aa66b91fafe966fe1f673dcf
wimafy <wimafy@gmail.com>
2016-08-10 18:56:19 -0700

    dcsa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,7 +35,7 @@
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>-->
         <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
         <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         
         
         
@@ -489,6 +489,8 @@
             }  
             
             
+        </script>
+        <script>
             
             (function (window, $) {
   
