4a62b5c837c21e9be74a1a323b12ca26e9ed788a
wimafy <wimafy@gmail.com>
2016-08-10 21:49:51 -0700

    css

--- public/js/test.js
+++ public/js/test.js
@@ -1,10 +1,10 @@
-( function( $ ) {
+( function( ripplefunction ) {
     $( function() {
 
-        $( '.ripple' ).ripple();
-        $( '.ripple-fast' ).ripple( { 'v_duration': 150, 'h_duration': 150 } );
-        $( '.ripple-slow' ).ripple( { 'v_duration': 1000, 'h_duration': 1000 } );
-        $( '.ripple-fast-slow' ).ripple( { 'v_duration': 600, 'h_duration': 150 } );
+        ripplefunction( '.ripple' ).ripple();
+        ripplefunction( '.ripple-fast' ).ripple( { 'v_duration': 150, 'h_duration': 150 } );
+        ripplefunction( '.ripple-slow' ).ripple( { 'v_duration': 1000, 'h_duration': 1000 } );
+        ripplefunction( '.ripple-fast-slow' ).ripple( { 'v_duration': 600, 'h_duration': 150 } );
 
     } );
 } )( jQuery );
