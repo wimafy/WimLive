af0465c81491b508d2c88f25e4676958dc883950
wimafy <wimafy@gmail.com>
2016-08-15 20:01:05 -0400

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -55,59 +55,6 @@
 
         </style>
         
-        <!-- search map script -->
-
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHNf3XccAk9z3UOtXD7LxwnMy0J8KOrik&libraries=places" type="text/javascript"></script>
-
-
-
-        <script>
-
-            var map = new google.maps.Map(document.getElementById('map-canvas'),{
-                center:{
-                    lat: 27.72,
-                    lng: 85.36
-                },
-                zoom:15
-            });
-
-            var marker = new google.maps.Marker({
-                position:{
-                    lat: 27.72,
-                    lng:85.36
-                },
-                map:map,
-                draggable:true
-            });
-
-            var searchBox = new google.maps.places.SearchBox(document.getElementById('mapsearch'));
-
-            //place cange event on search box
-            google.maps.event.addListener(searchBox, 'places_changed',function(){
-
-                console.log(searchBox.getPlaces());
-                var places = searchBox.getPlaces();
-
-                //bound
-                var bounds = new google.maps.LatLngBounds();
-                var i, place;
-
-                for(i=0; place=places[i];i++){
-
-                    // console.log(place.geometry.location);
-
-                    bounds.extend(place.geometry.location);
-                    marker.setPosition(place.geometry.location);
-
-                }
-
-                map.fitBounds(bounds);
-                map.setZoom(15);
-
-            })
-
-        </script>
-        
         
         
     </head>
@@ -539,7 +486,61 @@
             }  
             
         </script>
-  
+
+    <!-- search map script -->
+
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHNf3XccAk9z3UOtXD7LxwnMy0J8KOrik&libraries=places" type="text/javascript"></script>
+
+
+
+    <script>
+
+        var map = new google.maps.Map(document.getElementById('map-canvas'),{
+            center:{
+                lat: 27.72,
+                lng: 85.36
+            },
+            zoom:15
+        });
+
+        var marker = new google.maps.Marker({
+            position:{
+                lat: 27.72,
+                lng:85.36
+            },
+            map:map,
+            draggable:true
+        });
+
+        var searchBox = new google.maps.places.SearchBox(document.getElementById('mapsearch'));
+
+        //place cange event on search box
+        google.maps.event.addListener(searchBox, 'places_changed',function(){
+
+            console.log(searchBox.getPlaces());
+            var places = searchBox.getPlaces();
+
+            //bound
+            var bounds = new google.maps.LatLngBounds();
+            var i, place;
+
+            for(i=0; place=places[i];i++){
+
+                // console.log(place.geometry.location);
+
+                bounds.extend(place.geometry.location);
+                marker.setPosition(place.geometry.location);
+
+            }
+
+            map.fitBounds(bounds);
+            map.setZoom(15);
+
+        })
+
+    </script>
+        
+        
         
     </body>
     
