fb69033cd9c397c7d82ab2fb514491801e54a82c
wimafy <wimafy@gmail.com>
2016-08-17 18:33:56 -0400

    css

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -5,3 +5,25 @@ function showadvancedoptions() {
 function hideadvancedoptions() {
     document.getElementById("newwimadvancedoptionsdiv").style.left = "100vw";
 }
+
+
+function showmapsearch() {
+    document.getElementById("newwimmapsearchdiv").style.left = "0vw";
+}
+
+function hidemapsearch() {
+    document.getElementById("newwimmapsearchdiv").style.left = "100vw";
+}
+
+
+function showinvitefriends() {
+    document.getElementById("newwiminvitefriendsdiv").style.left = "0vw";
+}
+
+function hideinvitefriends() {
+    document.getElementById("newwiminvitefriendsdiv").style.left = "100vw";
+}
+
+
+
+
