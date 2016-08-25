5021442a6476d0a51556a88bceb8d031db826f61
wimafy <wimafy@gmail.com>
2016-08-25 17:02:07 -0400

    css

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -80,10 +80,14 @@ function newwimwimtype(wimtypeID) {
 //new wim v2
 
 function newwimadvanceslide(divID) {
-    document.getElementById(divID).style.left = "0vw";
+    //document.getElementById(divID).style.left = "0vw";
+    document.getElementById(divID).classList.add('pagetransitionIN');
+    document.getElementById(divID).classList.remove('pagetransitionOUT');
 }
 
 function newwimbackslide(divID) {
-    document.getElementById(divID).style.left = "120vw";
+    document.getElementById(divID).classList.remove('pagetransitionIN');
+    document.getElementById(divID).classList.add('pagetransitionOUT');
+    //document.getElementById(divID).style.left = "120vw";
 }
 
