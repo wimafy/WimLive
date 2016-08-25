81159f9c744cb4f145fc8c347c3417d15e608ddd
wimafy <wimafy@gmail.com>
2016-08-25 15:41:20 -0400

    css

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -1,45 +1,52 @@
 function showadvancedoptions() {
-    document.getElementById("newwimadvancedoptionsdiv").style.left = "0vw";
     document.getElementById("fullpage3newwim").style.zIndex = "49";
+    document.getElementById("newwimadvancedoptionsdiv").style.left = "0vw";
 }
 
 function hideadvancedoptions() {
-    document.getElementById("newwimadvancedoptionsdiv").style.left = "120vw";
     document.getElementById("fullpage3newwim").style.zIndex = "100";
+    document.getElementById("newwimadvancedoptionsdiv").style.left = "120vw";
+
 }
 
 
 function showmapsearch() {
-    document.getElementById("newwimmapsearchdiv").style.left = "0vw";
     document.getElementById("fullpage3newwim").style.zIndex = "49";
+    document.getElementById("newwimmapsearchdiv").style.left = "0vw";
+    
     
 }
 
 function hidemapsearch() {
-    document.getElementById("newwimmapsearchdiv").style.left = "120vw";
     document.getElementById("fullpage3newwim").style.zIndex = "100";
+    document.getElementById("newwimmapsearchdiv").style.left = "120vw";
+    
 }
 
 
 function showinvitefriends() {
-    document.getElementById("newwiminvitefriendsdiv").style.left = "0vw";
     document.getElementById("fullpage3newwim").style.zIndex = "49";
+    document.getElementById("newwiminvitefriendsdiv").style.left = "0vw";
+    
 }
 
 function hideinvitefriends() {
-    document.getElementById("newwiminvitefriendsdiv").style.left = "120vw";
     document.getElementById("fullpage3newwim").style.zIndex = "100";
+    document.getElementById("newwiminvitefriendsdiv").style.left = "120vw";
+    
 }
 
 
 function showwimwimtype() {
-    document.getElementById("newwimwimtypediv").style.left = "0vw";
     document.getElementById("fullpage3newwim").style.zIndex = "49";
+    document.getElementById("newwimwimtypediv").style.left = "0vw";
+    
 }
 
 function hidewimwimtype() {
-    document.getElementById("newwimwimtypediv").style.left = "120vw";
     document.getElementById("fullpage3newwim").style.zIndex = "100";
+    document.getElementById("newwimwimtypediv").style.left = "120vw";
+    
 }
 
 
