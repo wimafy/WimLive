38355ad3e238dfd5463c681291533f4598719682
wimafy <wimafy@gmail.com>
2016-08-26 13:24:05 -0400

    css

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -1,36 +1,52 @@
 function showadvancedoptions() {
+    document.getElementById("fullpage3newwim").style.zIndex = "49";
     document.getElementById("newwimadvancedoptionsdiv").style.left = "0vw";
 }
 
 function hideadvancedoptions() {
+    document.getElementById("fullpage3newwim").style.zIndex = "100";
     document.getElementById("newwimadvancedoptionsdiv").style.left = "120vw";
+
 }
 
 
 function showmapsearch() {
+    document.getElementById("fullpage3newwim").style.zIndex = "49";
     document.getElementById("newwimmapsearchdiv").style.left = "0vw";
+    
+    
 }
 
 function hidemapsearch() {
+    document.getElementById("fullpage3newwim").style.zIndex = "100";
     document.getElementById("newwimmapsearchdiv").style.left = "120vw";
+    
 }
 
 
 function showinvitefriends() {
+    document.getElementById("fullpage3newwim").style.zIndex = "49";
     document.getElementById("newwiminvitefriendsdiv").style.left = "0vw";
+    
 }
 
 function hideinvitefriends() {
+    document.getElementById("fullpage3newwim").style.zIndex = "100";
     document.getElementById("newwiminvitefriendsdiv").style.left = "120vw";
+    
 }
 
 
 function showwimwimtype() {
+    document.getElementById("fullpage3newwim").style.zIndex = "49";
     document.getElementById("newwimwimtypediv").style.left = "0vw";
+    
 }
 
 function hidewimwimtype() {
+    document.getElementById("fullpage3newwim").style.zIndex = "100";
     document.getElementById("newwimwimtypediv").style.left = "120vw";
+    
 }
 
 
@@ -61,3 +77,16 @@ function newwimwimtype(wimtypeID) {
     
 }
 
+//new wim v2
+
+function newwimadvanceslide(divID) {
+    //document.getElementById(divID).style.left = "0vw";
+    document.getElementById(divID).classList.add('pagetransitionIN');
+    document.getElementById(divID).classList.remove('pagetransitionOUT');
+}
+
+function newwimbackslide(divID) {
+    document.getElementById(divID).classList.remove('pagetransitionIN');
+    document.getElementById(divID).classList.add('pagetransitionOUT');
+    //document.getElementById(divID).style.left = "120vw";
+}
