486a10522d7be2703819ffc487b5a81de3051ef9
wimafy <wimafy@gmail.com>
2016-08-28 17:19:16 -0400

    csd

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -3,12 +3,23 @@ function indexpagebuttonnotify() {
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitioncurrentwims');
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnewwim');
     
+    
+    
+    document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition1');
+    document.getElementById("indexpagebottomdivid").classList.add('indexpagebottompagesliderdivposition2');
+    document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition3');
+    
+    
+    
+    
+    
     document.getElementById("indexpagebuttonpics1").style.color = "#2196F3";
     document.getElementById("indexpagebuttonpics12").style.color = "#2196F3";
     document.getElementById("indexpagebuttonpics2").style.color = "black";
     document.getElementById("indexpagebuttonpics22").style.color = "black";
     document.getElementById("indexpagebuttonpics3").style.color = "black";
     document.getElementById("indexpagebuttonpics32").style.color = "black";
+    
     
 }
 
