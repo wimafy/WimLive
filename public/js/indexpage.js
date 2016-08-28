525daf2cab28e1ce5c8dc087524d406e9ef4c83e
wimafy <wimafy@gmail.com>
2016-08-28 12:52:36 -0400

    csd

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -1,3 +1,16 @@
 function indexpagebuttonnotify() {
     alert("wweee");
+    //document.getElementById("interestsID").style.color = "#2196F3";
+    //document.getElementById("friendsID").style.color = "black";
+    //document.getElementById("contactinfoID").style.color = "black";
+    document.getElementById("fullpage3newwim").classList.add('indexpagebuttonhighlightertransitionnotify');
+    
+}
+
+function indexpagebuttoncurrentwims() {
+    alert("two");
+}
+
+function indexpagebuttonnewwim() {
+    alert("three");
 }
