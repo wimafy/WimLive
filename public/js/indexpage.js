5c27b74865282335e29a7d95fa54483888560883
wimafy <wimafy@gmail.com>
2016-08-31 23:59:36 -0400

    css

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -44,6 +44,33 @@ function indexpagebuttoncurrentwims() {
     document.getElementById("indexpagebuttonpics32").style.color = "#757575";
 }
 
+
+function indexpagebuttoncurrentwims() {
+    document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnotify');
+    document.getElementById("indexpagebuttonhighligher").classList.add('indexpagebuttonhighlightertransitioncurrentwims');
+    document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnewwim');
+    
+    
+    
+    document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition1');
+    document.getElementById("indexpagebottomdivid").classList.add('indexpagebottompagesliderdivposition2');
+    document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition3');
+    
+    
+    
+     document.getElementById("indexpagebuttonpics1").style.color = "#757575";
+    document.getElementById("indexpagebuttonpics12").style.color = "#757575";
+    document.getElementById("indexpagebuttonpics2").style.color = "#2196F3";
+    document.getElementById("indexpagebuttonpics22").style.color = "#2196F3";
+    document.getElementById("indexpagebuttonpics3").style.color = "#757575";
+    document.getElementById("indexpagebuttonpics32").style.color = "#757575";
+}
+
+
+indexpagebuttonhighlightertransitionsearch
+
+
+
 function indexpagebuttonnewwim() {
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnotify');
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitioncurrentwims');
