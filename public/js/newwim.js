bad5d2193050a5761e09f90e091c6b8e2af3fcec
wimafy <wimafy@gmail.com>
2016-09-05 16:11:00 -0400

    css

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -93,9 +93,13 @@ function newwimbackslide(divID) {
 
 function newwimdestinationpagein() {
     document.getElementById('newwimmapsearchdiv').style.left = "0vw";
+    document.getElementById('fullpage3newwim').style.z-index = "50";
+    
+    
 }
 
 
 function newwimdestinationpageout() {
      document.getElementById('newwimmapsearchdiv').style.left = "125vw";
+    document.getElementById('fullpage3newwim').style.z-index = "100";
 }
