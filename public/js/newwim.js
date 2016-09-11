366d7487b5ac80762c5031b2ca363af886edc1d2
wimafy <wimafy@gmail.com>
2016-09-11 02:09:25 -0400

    javascript

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -96,13 +96,14 @@ function newwimbackslide(divID) {
 function newwimdestinationpagein() {
     
     var ipaddressvar = geoplugin_request();
-    var latitudevar = geoplugin_latitude();
-    var longitudevar = geoplugin_longitude();
+    var latitudevar = Number(geoplugin_latitude()).toFixed(2);
+    var longitudevar = Number(geoplugin_longitude()).toFixed(2);
     console.log(ipaddressvar);
     console.log(latitudevar);
     console.log(longitudevar);
     var one = 27.72;
     var two = 85.36;
+    
     
     
     
