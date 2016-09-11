2bb5530d99a4f0395ec7936d1e203d0dac46964c
wimafy <wimafy@gmail.com>
2016-09-10 22:14:57 -0400

    css

--- public/js/modal.js
+++ public/js/modal.js
@@ -8,18 +8,18 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
+document.getElementById("myBtn").onclick = function() {
+    document.getElementById('myModal').style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+document.getElementsByClassName("close")[0].onclick = function() {
+    document.getElementById('myModal').style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
+        document.getElementById('myModal').style.display = "none";
     }
 }
