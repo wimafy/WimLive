b66c80c4ac4f96ff173a58cc93906774bab59058
wimafy <wimafy@gmail.com>
2016-09-10 23:13:41 -0400

    css

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,11 +1,14 @@
 function profilemodalcall() {
+    var modalbackground = document.getElementById("profilemodalbackground");
     var modal = document.getElementById("profilemodal");
  
+    modalbackground.style.zIndex = "100";
     modal.style.zIndex = "100";
 }
 
 function profilemodalaway() {
     var modal = document.getElementById("profilemodal");
  
+    modalbackground.style.zIndex = "-100";
     modal.style.zIndex = "-100";
 }
