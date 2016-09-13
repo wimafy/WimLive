15d758b4ae9f8ee3fe9deea8ae1ca7de4717b481
wimafy <wimafy@gmail.com>
2016-09-13 19:18:36 -0400

    css

--- public/js/indexpage.js
+++ public/js/indexpage.js
@@ -2,14 +2,14 @@ function indexpagebuttonnotify() {
     document.getElementById("indexpagebuttonhighligher").classList.add('indexpagebuttonhighlightertransitionnotify');
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitioncurrentwims');
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionsearch');
-    document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnewwim');
+    
     
     
     
     document.getElementById("indexpagebottomdivid").classList.add('indexpagebottompagesliderdivposition1');
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition2');
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition3');
-    document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition4');
+    
     
     
     
@@ -21,8 +21,7 @@ function indexpagebuttonnotify() {
     document.getElementById("indexpagebuttonpics22").style.color = "#757575";
     document.getElementById("indexpagebuttonpics3").style.color = "#757575";
     document.getElementById("indexpagebuttonpics32").style.color = "#757575";
-    document.getElementById("indexpagebuttonpics4").style.color = "#757575";
-    document.getElementById("indexpagebuttonpics42").style.color = "#757575";
+    
     
     
 }
@@ -31,14 +30,14 @@ function indexpagebuttoncurrentwims() {
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnotify');
     document.getElementById("indexpagebuttonhighligher").classList.add('indexpagebuttonhighlightertransitioncurrentwims');
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionsearch');
-    document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnewwim');
+    
     
     
     
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition1');
     document.getElementById("indexpagebottomdivid").classList.add('indexpagebottompagesliderdivposition2');
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition3');
-    document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition4');
+    
     
     
     
@@ -48,8 +47,7 @@ function indexpagebuttoncurrentwims() {
     document.getElementById("indexpagebuttonpics22").style.color = "#2196F3";
     document.getElementById("indexpagebuttonpics3").style.color = "#757575";
     document.getElementById("indexpagebuttonpics32").style.color = "#757575";
-    document.getElementById("indexpagebuttonpics4").style.color = "#757575";
-    document.getElementById("indexpagebuttonpics42").style.color = "#757575";
+  
 }
 
 
@@ -58,14 +56,13 @@ function indexpagebuttonsearch() {
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitioncurrentwims');
    
     document.getElementById("indexpagebuttonhighligher").classList.add('indexpagebuttonhighlightertransitionsearch');
-    document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnewwim');
+    
     
     
     
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition1');
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition2');
     document.getElementById("indexpagebottomdivid").classList.add('indexpagebottompagesliderdivposition3');
-    document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition4');
     
     
     
@@ -75,8 +72,7 @@ function indexpagebuttonsearch() {
     document.getElementById("indexpagebuttonpics22").style.color = "#757575";
     document.getElementById("indexpagebuttonpics3").style.color = "#2196F3";
     document.getElementById("indexpagebuttonpics32").style.color = "#2196F3";
-    document.getElementById("indexpagebuttonpics4").style.color = "#757575";
-    document.getElementById("indexpagebuttonpics42").style.color = "#757575";
+   
 }
 
 
@@ -89,13 +85,13 @@ function indexpagebuttonnewwim() {
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionnotify');
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitioncurrentwims');
     document.getElementById("indexpagebuttonhighligher").classList.remove('indexpagebuttonhighlightertransitionsearch');
-    document.getElementById("indexpagebuttonhighligher").classList.add('indexpagebuttonhighlightertransitionnewwim');
+    
     
     
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition1');
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition2');
     document.getElementById("indexpagebottomdivid").classList.remove('indexpagebottompagesliderdivposition3');
-    document.getElementById("indexpagebottomdivid").classList.add('indexpagebottompagesliderdivposition4');
+    
     
      document.getElementById("indexpagebuttonpics1").style.color = "#757575";
     document.getElementById("indexpagebuttonpics12").style.color = "#757575";
@@ -103,6 +99,5 @@ function indexpagebuttonnewwim() {
     document.getElementById("indexpagebuttonpics22").style.color = "#757575";
     document.getElementById("indexpagebuttonpics3").style.color = "#757575";
     document.getElementById("indexpagebuttonpics32").style.color = "#757575";
-    document.getElementById("indexpagebuttonpics4").style.color = "#2196F3";
-    document.getElementById("indexpagebuttonpics42").style.color = "#2196F3";
+    
 }
