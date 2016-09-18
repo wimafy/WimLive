753e69e9b86ce648a282840fde8f420945617bd6
wimafy <wimafy@gmail.com>
2016-09-17 23:38:38 -0400

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,8 +19,7 @@
         <script src="bower_components/angular-route/angular-route.min.js"></script>
         <script src="bower_components/angular-local-storage/dist/angular-local-storage.min.js"></script>
         <script src="bower_components/restangular/dist/restangular.min.js"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCyB6K1CFUQ1RwVJ-nyXxd6W0rfiIBe12Q&libraries=places&callback=initAutocomplete"
-         async defer type="text/javascript"></script>
+        
         <script src="https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCyB6K1CFUQ1RwVJ-nyXxd6W0rfiIBe12Q" type="text/javascript"></script>
         <script language="JavaScript" src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
@@ -37,7 +36,8 @@
         <script src = "js/signup.js"></script>
         <script src = "js/modal.js"></script>
         
-        
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCyB6K1CFUQ1RwVJ-nyXxd6W0rfiIBe12Q&libraries=places&callback=initAutocomplete"
+         async defer type="text/javascript"></script>
         
         <!--loading screen icon -->
         <!-- old jquery (it works though)
