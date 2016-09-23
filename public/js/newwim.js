0554d632cfba523d57235e68aedecd8d7e8e5726
wimafy <wimafy@gmail.com>
2016-09-23 10:44:53 -0400

    css

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -241,7 +241,7 @@ function initAutocomplete() {
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
                 //place 
-                alert(place.name);
+                alert(place.formatted_address);
                 infoWindow.setContent('ID: '+ place.id);
                 infoWindow.open(map, marker);
             }
