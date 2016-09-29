27b3a985b89872d7ac8ead25ed86a814f7253e18
wimafy <wimafy@gmail.com>
2016-09-29 16:16:00 -0400

    dropdown menue

--- public/js/profileActions.js
+++ public/js/profileActions.js
@@ -37,3 +37,23 @@ function showUserinfoTab() {
     document.getElementById("friendsID").style.color = "black";
     document.getElementById("contactinfoID").style.color = "#2196F3";
 }
+
+
+function profilepagedropdownFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
