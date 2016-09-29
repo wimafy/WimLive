4182f6aa65f64842b7267412a062a384beb2a231
wimafy <wimafy@gmail.com>
2016-09-29 00:57:46 -0400

    Revert "countdown timer maybe"
    This reverts commit c097c3b29458f62472262f9ceeecb49f50c178ad.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -138,6 +138,7 @@ wimControllers.controller('ProfileController', ['$scope', '$log', '$location', '
 	$scope.logout = function(){
 		userService.logout();
 		$location.path('/login');
+        alert("1");
         
         document.getElementById("ngviewdiv").classList.add('ngviewtransitionout');
         document.getElementById("fullpage2myprofile").classList.remove('pagetransitionIN');
@@ -398,38 +399,3 @@ wimControllers.controller('MainController', ['userService', '$location', '$scope
 		$location.path('/login');
 
 }]);
-
-
-//Countdown Timer
-//https://codepen.io/garethdweaver/pen/eNpWBb
-//Gareth Weaver
-
-angular.module 'countdownapp', []
-
-.directive 'countdown', ['Util', '$interval', (Util, $interval) ->
-  restrict: 'A'
-  scope:
-  	date: '@'
-  link: (scope, element) ->    
-    future = new Date scope.date
-    $interval ->
-      diff = Math.floor (future.getTime() - new Date().getTime()) / 1000
-      element.text  Util.dhms diff
-    , 1000
-    return
-]
-
-.factory 'Util', [ ->
-  {
-    dhms: (t) ->
-      days = Math.floor t / 86400
-      t -= days * 86400
-      hours = Math.floor(t / 3600) % 24
-      t -= hours * 3600
-      minutes = Math.floor(t / 60) % 60
-      t -= minutes * 60
-      seconds = t % 60
-      [ days + 'd', hours + 'h', minutes + 'm', seconds + 's' ].join ' '
-  }
-]
-
