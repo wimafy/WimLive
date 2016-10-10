a144197b27b2bd1dbdd8fee601fee480b779da7d
wimafy <wimafy@gmail.com>
2016-10-09 21:25:30 -0400

    css

--- public/js/newwim.js
+++ public/js/newwim.js
@@ -226,8 +226,7 @@ function initAutocomplete() {
               //document.getElementById("newwimdestinationbuttontext").innerHTML = place.formatted_address;
               document.getElementById("newwiminputdestinationID").innerHTML = place.name;
               document.getElementById("newwiminputdestinationaddressID").innerHTML = place.formatted_address;
-              document.getElementById("newwiminputdestinationaddressIDinput").value = "place.formatted_address";
-              
+              document.getElementById("myText").value = "Johnny 
               
           }  
               
