a3519d22dd042b85342cb6e45d5723bc5e0d0c6c
wimafy <wimafy@gmail.com>
2016-10-13 23:43:35 -0400

    css
    c

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -176,6 +176,36 @@ wimControllers.controller('ProfileController', ['$scope', '$log', '$location', '
 
 	$scope.profile = [];
 	$scope.refresh();
+    
+    
+    .directive 'countdown', ['Util', '$interval', (Util, $interval) ->
+      restrict: 'A'
+      scope:
+        date: '@'
+      link: (scope, element) ->    
+        future = new Date scope.date
+        $interval ->
+          diff = Math.floor (future.getTime() - new Date().getTime()) / 1000
+          element.text  Util.dhms diff
+        , 1000
+        return
+    ]
+
+    .factory 'Util', [ ->
+      {
+        dhms: (t) ->
+          days = Math.floor t / 86400
+          t -= days * 86400
+          hours = Math.floor(t / 3600) % 24
+          t -= hours * 3600
+          minutes = Math.floor(t / 60) % 60
+          t -= minutes * 60
+          seconds = t % 60
+          [ days + 'd', hours + 'h', minutes + 'm', seconds + 's' ].join ' '
+      }
+    ]
+
+    
 }]);
 
 wimControllers.controller('UserListController', ['$scope', '$location', 'userService', 'relationshipService', '$http', function ($scope, $location, userService, relationshipService, $http) {
@@ -295,10 +325,6 @@ wimControllers.controller('FriendController', ['$scope', '$location', 'userServi
             newwiminvitedfriends.push(IDnumber);
             //alert(newwiminvitedfriends);
         }
-
-
-        
-
 
     }
     
