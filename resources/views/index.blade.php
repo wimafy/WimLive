539d6a4c5603fc9ce717e9ecc158223e5ae06369
wimafy <wimafy@gmail.com>
2016-10-15 23:19:51 -0400

    countdowntime

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -35,7 +35,7 @@
         <script src = "js/indexpage.js"></script>
         <script src = "js/signup.js"></script>
         <script src = "js/modal.js"></script>
-        <script src = "js/contdowntime.js"></script>
+        <script src = "js/countdowntime.js"></script>
         
         
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCyB6K1CFUQ1RwVJ-nyXxd6W0rfiIBe12Q&libraries=places&callback=initAutocomplete"
