ef1ca57127328d8818a87ba99aed887625cbb3e6
wimafy <wimafy@gmail.com>
2016-11-10 17:48:07 -0500

    test

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -182,6 +182,90 @@ wimControllers.controller('ProfileController', ['$scope', '$log', '$location', '
 
 	}
 
+
+
+
+
+
+
+
+  $scope.getTimeRemaining = function(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+
+ $scope.initializeClock = function(id, clockID, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+
+    $scope.updateClock = function() {
+      var t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if (t.days > 1){
+          document.getElementById(clockID).innerHTML = t.days + " Days";
+      }else if (t.days > 0){
+          document.getElementById(clockID).innerHTML = t.days + " Day";
+      }else if (('0' + t.hours).slice(-2) > 1){
+           document.getElementById(clockID).innerHTML = ('0' + t.hours).slice(-2) + " Hours";
+      }else if (('0' + t.hours).slice(-2) > 0){
+           document.getElementById(clockID).innerHTML = ('0' + t.hours).slice(-2) + " Hour";
+      }else if (('0' + t.minutes).slice(-2) > 0){
+           document.getElementById(clockID).innerHTML = ('0' + t.minutes).slice(-2) + " Minutes";
+      }else if (('0' + t.seconds).slice(-2) > 0){
+           document.getElementById(clockID).innerHTML = "1 Minute";
+      }
+
+
+
+      if (t.total <= 0) {
+        //clearInterval(timeinterval);
+        document.getElementById(clockID).innerHTML = "Active Now";
+      }
+        console.log("update");
+
+
+    }
+
+
+   //var timeinterval = setInterval(updateClock(), 1);
+   $scope.updateClock()
+   setInterval( function(){$scope.updateClock()}, 5000);
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
   $scope.customers=  [
             { name:'John Smith', city:'Phoenix', id: 12345},
             { name:'John Doe', city:'New York', id: 67891},
