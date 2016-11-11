5939c7f963fcca255e61710df6967ba19018f25e
wimafy <wimafy@gmail.com>
2016-11-11 17:44:51 -0500

    css

--- public/js/directives/countdownclockDir.js
+++ public/js/directives/countdownclockDir.js
@@ -14,6 +14,7 @@ angular.module('wimControllers.directives.countdownClock', [])
           console.log($scope.data);
           $interval(function() {
             console.log('interval');
+            console.log(data.id);
           }, 1000);
         }
     };
