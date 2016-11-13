7bbc716490a27f82e39bc8296b198a11dff5b861
wimafy <wimafy@gmail.com>
2016-11-12 19:48:06 -0500

    css

--- public/js/directives/countdownclockDir.js
+++ public/js/directives/countdownclockDir.js
@@ -16,87 +16,10 @@ angular.module('wimControllers.directives.countdownClock', [])
         controller: function($scope, $interval) {
           console.log($scope.data);
           //interval is set at the end of this function
-        //  $interval(function() {
+         $interval(function() {
             console.log('interval');
             console.log($scope.data.id);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-            function getTimeRemaining(endtime) {
-              var t = Date.parse(endtime) - Date.parse(new Date());
-              var seconds = Math.floor((t / 1000) % 60);
-              var minutes = Math.floor((t / 1000 / 60) % 60);
-              var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-              var days = Math.floor(t / (1000 * 60 * 60 * 24));
-              return {
-                'total': t,
-                'days': days,
-                'hours': hours,
-                'minutes': minutes,
-                'seconds': seconds
-              };
-            }
-
-
-           function initializeClock(id, clockID, endtime) {
-              var clock = document.getElementById(id);
-              var daysSpan = clock.querySelector('.days');
-              var hoursSpan = clock.querySelector('.hours');
-              var minutesSpan = clock.querySelector('.minutes');
-              var secondsSpan = clock.querySelector('.seconds');
-
-
-              function updateClock() {
-                var t = getTimeRemaining(endtime);
-
-                daysSpan.innerHTML = t.days;
-                hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-                minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-                secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-                if (t.days > 1){
-                    document.getElementById(clockID).innerHTML = t.days + " Days";
-                }else if (t.days > 0){
-                    document.getElementById(clockID).innerHTML = t.days + " Day";
-                }else if (('0' + t.hours).slice(-2) > 1){
-                     document.getElementById(clockID).innerHTML = ('0' + t.hours).slice(-2) + " Hours";
-                }else if (('0' + t.hours).slice(-2) > 0){
-                     document.getElementById(clockID).innerHTML = ('0' + t.hours).slice(-2) + " Hour";
-                }else if (('0' + t.minutes).slice(-2) > 0){
-                     document.getElementById(clockID).innerHTML = ('0' + t.minutes).slice(-2) + " Minutes";
-                }else if (('0' + t.seconds).slice(-2) > 0){
-                     document.getElementById(clockID).innerHTML = "1 Minute";
-                }
-
-
-
-                if (t.total <= 0) {
-                  //clearInterval(timeinterval);
-                  document.getElementById(clockID).innerHTML = "Active Now";
-                }
-                  console.log("update");
-
-
-              }
-
-
-             //var timeinterval = setInterval(updateClock(), 1);
-             updateClock()
-             setInterval( function(){updateClock()}, 5000);
-
-
-            }
 
 
 
@@ -126,13 +49,11 @@ angular.module('wimControllers.directives.countdownClock', [])
 
             //var myVariable = "pawowk!";
 
-            initializeClock($scope.data.id, $scope.data.id, 'november 10 2016 17:55:59 GMT-0500')
-
             var bimpert = makething($scope.data.id);
 
             //actually changes text to whatever variable is set
-            //document.getElementById($scope.data.id).innerHTML = bimpert.variable;
-        //  }, 5000);
+            document.getElementById($scope.data.id).innerHTML = bimpert.variable;
+          }, 5000);
         }
     };
 });
