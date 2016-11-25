e31052cc55e70d294a9e79bb77e598280f77df02
wimafy <wimafy@gmail.com>
2016-11-25 00:32:30 -0500

    cdas

--- public/LightsApp/controller.js
+++ public/LightsApp/controller.js
@@ -27,19 +27,25 @@ app.service('getcurrenttime', function() {
 app.controller('myCtrl', function($scope, $timeout, $interval, getcurrenttime, $rootScope, $http) {
 
     //$http.get('http://127.0.0.1/BraxAttack.github.io/LightsApp/sysdate.php')
+    //sets date of current time on machine (next two lines)
     $scope.currenttimeLocalinit = new Date();
     $scope.currenttimeLocal = $scope.currenttimeLocalinit.getTime();
+    //pulls data from server
     $http.get('http://52.41.37.153/sysdate.php')
     .success(function(response) {
+
+      //gets time from server in millsec from epoc
       $scope.phprespone = response;
-
+      //sets variable as root so the variable can be accessed and added to
       $rootScope.currenttime2 = Number(response);
-
+      //no idea lol
       $scope.updatetime2;
-
+      //gets second time on machine
       $scope.currenttimeLocalinit2 = new Date();
       $scope.currenttimeLocal2 = $scope.currenttimeLocalinit2.getTime();
+      //first time on machine taken minus second time devided by 2 == one way trip
       $scope.timedifference = ($scope.currenttimeLocal2 - $scope.currenttimeLocal)/2;
+      //one way trip time is added to the server returned time
       $scope.currenttimeadded = Number($rootScope.currenttime2) + Number($scope.timedifference);
 
       //alert("wee");
