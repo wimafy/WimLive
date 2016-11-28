d0d35d3007cd5684259751c9c0c2493ed5055e85
wimafy <wimafy@gmail.com>
2016-11-28 01:05:29 -0500

    css

--- public/LightsApp/controller.js
+++ public/LightsApp/controller.js
@@ -243,7 +243,7 @@ app.controller('myCtrl', function($scope, $timeout, $interval, $scope, $http) {
     ]
 
     //millisecond that show will start
-    $scope.lightsChangeArrayInitTime =   1480312726484    ;
+    $scope.lightsChangeArrayInitTime =   1480313257392     ;
 
     //only fires if booliean is true
     $scope.booliean = true;
