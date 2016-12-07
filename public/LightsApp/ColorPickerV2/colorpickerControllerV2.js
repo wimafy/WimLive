901beafbb8cae8eec6efa5a7357803d2013bfcba
wimafy <wimafy@gmail.com>
2016-12-07 00:36:54 -0500

    get it boi, looking better bud

--- public/LightsApp/ColorPickerV2/colorpickerControllerV2.js
+++ public/LightsApp/ColorPickerV2/colorpickerControllerV2.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', []);
 app.controller('SettupController', function($scope, $timeout, $interval, $scope, $http) {
 
 $scope.frames = [
-["one","two","three"],["four","five","six"],["seven","eight","nine"]
+
 ];
 
 $scope.columns = [
@@ -14,30 +14,56 @@ $scope.row = [
 
 ];
 
-
-
-function printvar(x) {
-
-   //document.write("Info:" + x + "<br />");
-   var myname = x.split("_");
-   //document.write("Frame:" + myname[0] + " Row:"+ myname[1] + "<br />");
-   //$scope.bigarray[this.frame][this.row][this.cell]= "";
-
-
+$scope.addTimeMillsec = function() {
+  //alert("things");
+  $scope.frames.push([$scope.columns])
 }
 
-
-   for (row=0; row<=10; row++) {
+for(frame=0; frame<10; frame++){
+  $scope.frames.push([])
+   for (row=0; row<38; row++) {
      //console.log(row);
      //$scope.frames[this.frame].push(row);
-     $scope.columns.push([]);
-     for (seat=0; seat<=10; seat++) {
+     $scope.frames[frame].push([]);
+     for (seat=0; seat< 38; seat++) {
        //printvar("" + row + "_" + seat + "");
-       $scope.columns[row].push([seat]);
+       $scope.frames[frame][row].push([]);
+       $scope.frames[frame][row][seat] = 0;
      }
    }
 
+   //holder for time
+   //$scope.frames[frame].push([]);
+   $scope.frames[frame][38] = "100";
+ }
 
+$scope.colorListArray = ["", "red", "yellow", "green", "orange", "brown", "pink", "maroon"];
+
+
+//create temp div and do the lookup in there
+$scope.currentFrame = 0;
+
+$scope.setCurrentFrame = function(indexvar) {
+
+  $scope.currentFrame = indexvar;
+  //itterates through squares in frame
+  for (row=0; row<38; row++) {
+
+
+    for (seat=0; seat< 38; seat++) {
+
+      document.getElementById('display'+row+"."+seat).style.backgroundColor = $scope.colorListArray[$scope.frames[indexvar][row][seat]];
+    }
+  }
+
+}
+
+$scope.changeSquareColor = function(row, seat) {
+  //alert(String(row)+ String(seat));
+  $scope.frames[$scope.currentFrame][row][seat] = 5;
+  $scope.setCurrentFrame($scope.currentFrame);
+
+}
 
 
 });
